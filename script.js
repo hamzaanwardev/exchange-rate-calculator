79548a1d60fa78f50ef43f8c04baf37ae6ad291b
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-10-05 23:59:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const amountEl_two = document.getElementById('amount-two');
 
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
+
+function calculate () {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+}
